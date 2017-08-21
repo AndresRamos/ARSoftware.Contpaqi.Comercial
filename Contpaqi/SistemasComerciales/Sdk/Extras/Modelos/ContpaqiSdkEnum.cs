37c649e6e5f9150d39b4edd63a5c2b2cbd84bb26
Andres Ramos <gerardoaramos@outlook.com>
2017-08-21 13:42:23 -0500

    Se crearon una frabrica de servicios de sdk y se agrego el servicio de sdk para factura electronica

---
+++ Contpaqi/SistemasComerciales/Sdk/Extras/Modelos/ContpaqiSdkEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Contpaqi.SistemasComerciales.Sdk.Extras.Modelos
+{
+    public enum ContpaqiSdkEnum
+    {
+        Comercial = 0,
+        Adminpaq = 1,
+        FacturaElectronica = 2
+    }
+}
---
+++ Contpaqi/SistemasComerciales/Sdk/Extras/Servicios/FabricaContpaqiSdk.cs
@@ -0,0 +1,30 @@
+﻿using Contpaqi.SistemasComerciales.Sdk.Extras.Interfaces;
+using Contpaqi.SistemasComerciales.Sdk.Extras.Modelos;
+
+namespace Contpaqi.SistemasComerciales.Sdk.Extras.Servicios
+{
+    public class FabricaContpaqiSdk
+    {
+        public FabricaContpaqiSdk()
+        {
+        }
+
+        public ISistemasComercialesSdk Crear(ContpaqiSdkEnum sdk)
+        {
+            switch (sdk)
+            {
+                case ContpaqiSdkEnum.Comercial:
+                    return new ServicioComercialSdk();
+
+                case ContpaqiSdkEnum.Adminpaq:
+                    return new ServicioAdminpaqSdk();
+
+                case ContpaqiSdkEnum.FacturaElectronica:
+                    return new ServicioFacturaElectronicaSdk();
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
