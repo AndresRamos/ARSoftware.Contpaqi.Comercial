bf71ba498ad7f1439d176210529eea34a9970c11
Andres Ramos <andres@arsoft.net>
2020-01-03 17:02:29 -0600

    Se crearon los paquetes de Nuget Contpaqi.Sql.Nominas.Generales y Contpaqi.Sql.Nominas.Empresa

--- Contpaqi.Sql.Nominas.Generales/Properties/AssemblyInfo.cs
+++ Contpaqi.Sql.Nominas.Generales/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Contpaqi.Sql.Nominas.Generales")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Modelo de datos para las tablas generales de Contpaqi Nominas.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AR Software")]
 [assembly: AssemblyProduct("Contpaqi.Sql.Nominas.Generales")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © AR Software 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
