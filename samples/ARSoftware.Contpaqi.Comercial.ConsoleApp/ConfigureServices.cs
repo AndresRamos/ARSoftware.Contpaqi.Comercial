7c43d09fc46af05fa5243b2a38a7d4eef0d2ef1d
Andres Ramos <andres@arsoft.net>
2022-09-11 17:27:32 -0500

    feat: se agrega ejemplo de consola

---
+++ samples/ARSoftware.Contpaqi.Comercial.ConsoleApp/ConfigureServices.cs
@@ -0,0 +1,80 @@
+﻿using ARSoftware.Contpaqi.Comercial.Sdk.Extras;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Interfaces;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Models;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Repositories;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ARSoftware.Contpaqi.Comercial.ConsoleApp;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddSdkServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IContpaqiSdk, ComercialSdkExtended>();
+
+        // Agentes
+        services.AddSingleton(typeof(IAgenteRepository<>), typeof(AgenteRepository<>));
+        services.AddSingleton<IAgenteService, AgenteService>();
+
+        // Almacenes
+        services.AddSingleton(typeof(IAlmacenRepository<>), typeof(AlmacenRepository<>));
+        services.AddSingleton<IAlmacenService, AlmacenService>();
+
+        // Clasificaciones
+        services.AddSingleton(typeof(IClasificacionRepository<>), typeof(ClasificacionRepository<>));
+        services.AddSingleton<IClasificacionService, ClasificacionService>();
+
+        //Clientes
+        services.AddSingleton(typeof(IClienteProveedorRepository<>), typeof(ClienteProveedorRepository<>));
+        services.AddSingleton<IClienteProveedorService, ClienteProveedorService>();
+
+        // Conceptos
+        services.AddSingleton(typeof(IConceptoDocumentoRepository<>), typeof(ConceptoDocumentoRepository<>));
+        services.AddSingleton<IConceptoDocumentoService, ConceptoDocumentoService>();
+
+        // Datos CFDI
+        services.AddSingleton<IDatosCfdiRepository, DatosCfdiRepository>();
+
+        // Direcciones
+        services.AddSingleton(typeof(IDireccionRepository<>), typeof(DireccionRepository<>));
+        services.AddSingleton<IDireccionService, DireccionService>();
+
+        // Documentos
+        services.AddSingleton(typeof(IDocumentoRepository<>), typeof(DocumentoRepository<>));
+        services.AddSingleton<IDocumentoService, DocumentoService>();
+
+        // Empresas
+        services.AddSingleton<IEmpresaRepository<Empresa>, EmpresaRepository>();
+
+        // Monedas
+        services.AddSingleton<IMonedaRepository<Moneda>, MonedaRepository>();
+
+        // Errores
+        services.AddSingleton<ISdkErrorRepository<SdkError>, SdkErrorRepository>();
+
+        // Movimientos
+        services.AddSingleton(typeof(IMovimientoRepository<>), typeof(MovimientoRepository<>));
+        services.AddSingleton<IMovimientoService, MovimientoService>();
+
+        // Parametros
+        services.AddSingleton(typeof(IParametrosRepository<>), typeof(ParametrosRepository<>));
+
+        // Productos
+        services.AddSingleton(typeof(IProductoRepository<>), typeof(ProductoRepository<>));
+        services.AddSingleton<IProductoService, ProductoService>();
+
+        // Sesion
+        services.AddSingleton<IComercialSdkSesionService, ComercialSdkSesionService>();
+
+        // Unidades
+        services.AddSingleton(typeof(IUnidadMedidaRepository<>), typeof(UnidadMedidaRepository<>));
+        services.AddSingleton<IUnidadMedidaService, UnidadMedidaService>();
+
+        // Valores Clasificacion
+        services.AddSingleton(typeof(IValorClasificacionRepository<>), typeof(ValorClasificacionRepository<>));
+        services.AddSingleton<IValorClasificacionService, ValorClasificacionService>();
+
+        return services;
+    }
+}
---
+++ samples/ARSoftware.Contpaqi.Comercial.ConsoleApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using ARSoftware.Contpaqi.Comercial.ConsoleApp;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Interfaces;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder(args)
+                 .ConfigureServices(collection =>
+                 {
+                     // Agregar servicios del SDK
+                     collection.AddSdkServices();
+                 })
+                 .Build();
+
+await host.StartAsync();
+
+try
+{
+    // Iniciar sesion
+    Console.WriteLine("Iniciando sesion.");
+    var comercialSdkSesionService = host.Services.GetRequiredService<IComercialSdkSesionService>();
+    comercialSdkSesionService.IniciarSesionSdk("SUPERVISOR", "");
+
+    // Buscar y seleccionar empresa
+    Console.WriteLine("Buscando empresas.");
+    var empresasRepository = host.Services.GetRequiredService<IEmpresaRepository<Empresa>>();
+    List<Empresa> empresas = empresasRepository.TraerTodo().OrderBy(e => e.CNOMBREEMPRESA).ToList();
+
+    Console.WriteLine("EMPRESAS:");
+    foreach (Empresa empresa in empresas)
+        Console.WriteLine($"ID:{empresa.CIDEMPRESA} NOMBRE:{empresa.CNOMBREEMPRESA} RUTA:{empresa.CRUTADATOS}");
+    Console.Write("Seleccionar empresa ingresando el id: ");
+
+    string? empresaIdInput = Console.ReadLine();
+    bool parseResult = int.TryParse(empresaIdInput, out int empresaId);
+    if (!parseResult)
+        throw new InvalidOperationException("El id ingreso no es un numero valido.");
+
+    Empresa? empresaSeleccionada = empresas.FirstOrDefault(e => e.CIDEMPRESA == empresaId);
+    if (empresaSeleccionada is null)
+        throw new InvalidOperationException("El id no pertenece a una empresa en la lista.");
+
+    Console.WriteLine($"Empresa {empresaSeleccionada.CNOMBREEMPRESA} seleccionada.");
+
+    // Abrir empresa
+    Console.WriteLine("Abriendo empresa.");
+    comercialSdkSesionService.AbrirEmpresa(empresaSeleccionada.CRUTADATOS);
+
+    // Listar productos
+    Console.WriteLine("Buscando productos.");
+    var productoRepository = host.Services.GetRequiredService<IProductoRepository<Producto>>();
+
+    Console.WriteLine("PRODUCTOS:");
+    foreach (Producto producto in productoRepository.TraerTodo())
+        Console.WriteLine($"ID:{producto.CIDPRODUCTO} CODIGO:{producto.CCODIGOPRODUCTO} NOMBRE:{producto.CNOMBREPRODUCTO}");
+
+    // Cerrar empresa
+    Console.WriteLine("Cerrando empresa.");
+    comercialSdkSesionService.CerrarEmpresa();
+
+    // Terminar session
+    Console.WriteLine("Cerrando sesion.");
+    comercialSdkSesionService.TerminarSesionSdk();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
+finally
+{
+    await host.StopAsync();
+}
