dbe0d1cdf400f5aae31771c2a46a98702568fb17
Andres Ramos <andres@arsoft.net>
2022-11-05 02:47:50 -0500

    feat: agrega las constantes utilizadas en la funcion fSetNombrePAQ utilizadas por el SDK al iniciar una conexion

---
+++ src/ARSoftware.Contpaqi.Comercial.Sdk/Constantes/NombresPaqSdk.cs
@@ -0,0 +1,20 @@
+﻿namespace ARSoftware.Contpaqi.Comercial.Sdk.Constantes
+{
+    public static class NombresPaqSdk
+    {
+        /// <summary>
+        ///     Valor del parametro de la funcion fSetNombrePAQ del SDK de CONTPAQi Comercial.
+        /// </summary>
+        public const string Comercial = "CONTPAQ I COMERCIAL";
+
+        /// <summary>
+        ///     Valor del parametro de la funcion fSetNombrePAQ del SDK de CONTPAQi Adminpaq.
+        /// </summary>
+        public const string Adminpaq = "AdminPAQ";
+
+        /// <summary>
+        ///     Valor del parametro de la funcion fSetNombrePAQ del SDK de CONTPAQi Factura Electronica.
+        /// </summary>
+        public const string FacturaElectronica = "CONTPAQ I Facturacion";
+    }
+}
