e7076611f2bc994a448a42f51f280c062b72fec5
Andres Ramos <andres@arsoft.net>
2022-11-26 02:44:29 -0600

    feat: agrega constantes de los formatos de fecha utilizados por el SDK

---
+++ src/ARSoftware.Contpaqi.Comercial.Sdk/Constantes/FormatosFechaSdk.cs
@@ -0,0 +1,20 @@
+﻿namespace ARSoftware.Contpaqi.Comercial.Sdk.Constantes
+{
+    public static class FormatosFechaSdk
+    {
+        /// <summary>
+        ///     Formato para asignar fechas con el SDK.
+        /// </summary>
+        public const string Fecha = "MM/dd/yyyy";
+
+        /// <summary>
+        ///     Formato para leer fechas del SDK de Comercial
+        /// </summary>
+        public const string Comercial = "MM/dd/yyyy HH:mm:ss:fff";
+
+        /// <summary>
+        ///     Formato para leer fechas del SDK de Factura Electronica
+        /// </summary>
+        public const string FacturaElectronica = "M/d/yyyy HH:mm:ss:fff";
+    }
+}
