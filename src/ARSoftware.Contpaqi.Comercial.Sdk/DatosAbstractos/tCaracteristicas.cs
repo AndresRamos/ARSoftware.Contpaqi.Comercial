08ee26aaae9ecad2148fb7da93f2526e46852c1e
Andres Ramos <andres@arsoft.net>
2022-11-26 04:27:26 -0600

    docs: documenta tCaracteristicas

--- src/ARSoftware.Contpaqi.Comercial.Sdk/DatosAbstractos/tCaracteristicas.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sdk/DatosAbstractos/tCaracteristicas.cs
@@ -1,9 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using ARSoftware.Contpaqi.Comercial.Sdk.Constantes;
 
-// ReSharper disable UnusedType.Global
-// ReSharper disable FieldCanBeMadeReadOnly.Global
-// ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable InconsistentNaming
 
 namespace ARSoftware.Contpaqi.Comercial.Sdk.DatosAbstractos
@@ -11,14 +8,26 @@ namespace ARSoftware.Contpaqi.Comercial.Sdk.DatosAbstractos
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
     public struct tCaracteristicas
     {
+        /// <summary>
+        ///     Unidades del movimiento.
+        /// </summary>
         public double aUnidades;
 
+        /// <summary>
+        ///     Valor de la caracteristica 1 del movimiento.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongDescripcion)]
         public string aValorCaracteristica1;
 
+        /// <summary>
+        ///     Valor de la caracteristica 2 del movimiento.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongDescripcion)]
         public string aValorCaracteristica2;
 
+        /// <summary>
+        ///     Valor de la caracteristica 3 del movimiento.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongDescripcion)]
         public string aValorCaracteristica3;
     }
