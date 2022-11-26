818d12d09a352a3c5f5932b60a44587a18fa211b
Andres Ramos <andres@arsoft.net>
2022-11-26 02:54:02 -0600

    feat: agrega constantes utilizadas en la funcion fInicializaLicenseInfo

---
+++ src/ARSoftware.Contpaqi.Comercial.Sdk/Constantes/LicenseInfoSdk.cs
@@ -0,0 +1,12 @@
+﻿namespace ARSoftware.Contpaqi.Comercial.Sdk.Constantes
+{
+    /// <summary>
+    ///     Constantes utilizadas en la funcion fInicializaLicenseInfo(byte aSistema)
+    /// </summary>
+    public static class LicenseInfoSdk
+    {
+        public const int Adminpaq = 0;
+
+        public const int FacturaElectronica = 1;
+    }
+}
