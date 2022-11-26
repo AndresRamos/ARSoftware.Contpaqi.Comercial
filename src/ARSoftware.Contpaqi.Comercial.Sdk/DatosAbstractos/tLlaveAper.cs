36f0c83dc8c94e2e1882c474af013ca4a4f0e849
Andres Ramos <andres@arsoft.net>
2022-11-26 04:30:46 -0600

    Revert "Revert "docs: documenta tCteProv"" Agrega documentacion a las estructuras
    This reverts commit dd68392e0cf722612ed005070d15d2062389a14e.

--- src/ARSoftware.Contpaqi.Comercial.Sdk/DatosAbstractos/tLlaveAper.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sdk/DatosAbstractos/tLlaveAper.cs
@@ -1,9 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using ARSoftware.Contpaqi.Comercial.Sdk.Constantes;
 
-// ReSharper disable UnusedType.Global
-// ReSharper disable FieldCanBeMadeReadOnly.Global
-// ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable InconsistentNaming
 
 namespace ARSoftware.Contpaqi.Comercial.Sdk.DatosAbstractos
@@ -11,9 +8,15 @@ namespace ARSoftware.Contpaqi.Comercial.Sdk.DatosAbstractos
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
     public struct tLlaveAper
     {
+        /// <summary>
+        ///     Código de la caja.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongCodigo)]
         public string aCodCaja;
 
+        /// <summary>
+        ///     Fecha de apertura.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongFecha)]
         public string aFechaApe;
     }
--- src/ARSoftware.Contpaqi.Comercial.Sdk/DatosAbstractos/tValorClasificacion.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sdk/DatosAbstractos/tValorClasificacion.cs
@@ -1,9 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using ARSoftware.Contpaqi.Comercial.Sdk.Constantes;
 
-// ReSharper disable UnusedType.Global
-// ReSharper disable FieldCanBeMadeReadOnly.Global
-// ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable InconsistentNaming
 
 namespace ARSoftware.Contpaqi.Comercial.Sdk.DatosAbstractos
@@ -11,12 +8,25 @@ namespace ARSoftware.Contpaqi.Comercial.Sdk.DatosAbstractos
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
     public struct tValorClasificacion
     {
+        /// <summary>
+        ///     Clasificación.
+        /// </summary>
         public int cClasificacionDe;
+
+        /// <summary>
+        ///     Número de la clasificación.
+        /// </summary>
         public int cNumClasificacion;
 
+        /// <summary>
+        ///     Código del valor de la clasificación.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongCodValorClasif)]
         public string cCodigoValorClasificacion;
 
+        /// <summary>
+        ///     Valor de la clasificación.
+        /// </summary>
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = SdkConstantes.kLongDescripcion)]
         public string cValorClasificacion;
     }
