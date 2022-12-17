fc2ebc2ea83fc1cd9a45277b30894a924c5df225
Andres Ramos <andres@arsoft.net>
2022-12-16 23:02:51 -0600

    fix: deleted TipoContpaqiSdk from sdk extras because its duplicate

--- src/ARSoftware.Contpaqi.Comercial.Sdk.Extras/Models/Enums/TipoContpaqiSdk.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ARSoftware.Contpaqi.Comercial.Sdk.Extras.Models.Enums
-{
-    public enum TipoContpaqiSdk
-    {
-        Comercial = 0,
-        Adminpaq = 1,
-        FacturaElectronica = 2
-    }
-}
