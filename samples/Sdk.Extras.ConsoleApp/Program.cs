bfa5eb5f7fc78120e47877b3078636067f2040b5
Andres Ramos <andres@arsoft.net>
2022-12-21 05:57:49 -0600

    registra servicios con el proveedor de servicios

--- samples/Sdk.Extras.ConsoleApp/Program.cs
+++ samples/Sdk.Extras.ConsoleApp/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ARSoftware.Contpaqi.Comercial.Sdk;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Interfaces;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices(collection => { collection.AddContpaqiComercialSdkServices(TipoContpaqiSdk.Comercial); })
+    .Build();
+
+await host.StartAsync();
+
+await host.StopAsync();
