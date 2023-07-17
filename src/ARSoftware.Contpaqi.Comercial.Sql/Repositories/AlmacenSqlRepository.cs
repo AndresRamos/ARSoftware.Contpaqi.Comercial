191218da83b6ece7764fea32ff3abbb138279719
Andres Ramos <andres@arsoft.net>
2023-07-17 16:34:19 -0500

    feat: crea el repositorio sql de almacenes

---
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/AlmacenSqlRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
+using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
+using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+
+namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
+
+public sealed class AlmacenSqlRepository : IAlmacenRepository<admAlmacenes>
+{
+    private readonly ContpaqiComercialEmpresaDbContext _context;
+
+    public AlmacenSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc />
+    public admAlmacenes BuscarPorCodigo(string codigoAlmacen)
+    {
+        return _context.admAlmacenes.SingleOrDefault(almacen => almacen.CCODIGOALMACEN == codigoAlmacen);
+    }
+
+    /// <inheritdoc />
+    public admAlmacenes BuscarPorId(int idAlmacen)
+    {
+        return _context.admAlmacenes.SingleOrDefault(almacen => almacen.CIDALMACEN == idAlmacen);
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<admAlmacenes> TraerTodo()
+    {
+        return _context.admAlmacenes.ToList();
+    }
+}
