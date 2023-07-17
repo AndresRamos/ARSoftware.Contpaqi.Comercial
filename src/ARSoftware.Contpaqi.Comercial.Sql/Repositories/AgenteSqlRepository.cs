198ec33a3139fb236b691c5441a92e2d69e2c517
Andres Ramos <andres@arsoft.net>
2023-07-17 16:31:19 -0500

    feat: crea el repositorio de sql de agentes

---
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/AgenteSqlRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
+using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
+using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+
+namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
+
+public sealed class AgenteSqlRepository : IAgenteRepository<admAgentes>
+{
+    private readonly ContpaqiComercialEmpresaDbContext _context;
+
+    public AgenteSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc />
+    public admAgentes BuscarPorCodigo(string codigoAgente)
+    {
+        return _context.admAgentes.SingleOrDefault(agente => agente.CCODIGOAGENTE == codigoAgente);
+    }
+
+    /// <inheritdoc />
+    public admAgentes BuscarPorId(int idAgente)
+    {
+        return _context.admAgentes.SingleOrDefault(agente => agente.CIDAGENTE == idAgente);
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<admAgentes> TraerTodo()
+    {
+        return _context.admAgentes.ToList();
+    }
+}
