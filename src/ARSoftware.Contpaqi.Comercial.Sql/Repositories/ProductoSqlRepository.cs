2007baae81d7e933b5f565b5d7d89efaa37aca7e
Andres Ramos <andres@arsoft.net>
2023-07-17 19:03:19 -0500

    feat: crea el repositorio de sql de productos

---
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ProductoSqlRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Enums;
+using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
+using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
+using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+
+namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
+
+public sealed class ProductoSqlRepository : IProductoRepository<admProductos>
+{
+    private readonly ContpaqiComercialEmpresaDbContext _context;
+
+    public ProductoSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <inheritdoc />
+    public admProductos BuscarPorCodigo(string codigoProducto)
+    {
+        return _context.admProductos.SingleOrDefault(producto => producto.CCODIGOPRODUCTO == codigoProducto);
+    }
+
+    /// <inheritdoc />
+    public admProductos BuscarPorId(int idProducto)
+    {
+        return _context.admProductos.SingleOrDefault(producto => producto.CIDPRODUCTO == idProducto);
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<admProductos> TraerPorTipo(TipoProducto tipoProducto)
+    {
+        return _context.admProductos.Where(producto => producto.CTIPOPRODUCTO == (int)tipoProducto).ToList();
+    }
+
+    /// <inheritdoc />
+    public IEnumerable<admProductos> TraerTodo()
+    {
+        return _context.admProductos.ToList();
+    }
+}
