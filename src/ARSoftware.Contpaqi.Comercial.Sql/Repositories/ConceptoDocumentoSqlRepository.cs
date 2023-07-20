1fa813ae7afbf5d7eea89f9a28a260edd5909b55
Andres Ramos <andres@arsoft.net>
2023-07-20 01:28:37 -0500

    feat(sql): crea especificaciones de sql de conceptos

--- src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ConceptoDocumentoSqlRepository.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ConceptoDocumentoSqlRepository.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Ardalis.Specification.EntityFrameworkCore;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
 using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
 using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+using ARSoftware.Contpaqi.Comercial.Sql.Specifications;
 
 namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
 
-public sealed class ConceptoDocumentoSqlRepository : IConceptoDocumentoRepository<admConceptos>
+public sealed class ConceptoDocumentoSqlRepository : RepositoryBase<admConceptos>, IConceptoDocumentoRepository<admConceptos>
 {
     private readonly ContpaqiComercialEmpresaDbContext _context;
 
-    public ConceptoDocumentoSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    public ConceptoDocumentoSqlRepository(ContpaqiComercialEmpresaDbContext context) : base(context)
     {
         _context = context;
     }
@@ -18,19 +20,19 @@ public sealed class ConceptoDocumentoSqlRepository : IConceptoDocumentoRepositor
     /// <inheritdoc />
     public admConceptos BuscarPorCodigo(string codigoConcepto)
     {
-        return _context.admConceptos.SingleOrDefault(concepto => concepto.CCODIGOCONCEPTO == codigoConcepto);
+        return _context.admConceptos.WithSpecification(new ConceptoPorCodigoSpecification(codigoConcepto)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public admConceptos BuscarPorId(int idConcepto)
     {
-        return _context.admConceptos.SingleOrDefault(concepto => concepto.CIDCONCEPTODOCUMENTO == idConcepto);
+        return _context.admConceptos.WithSpecification(new ConceptoPorIdSpecification(idConcepto)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public IEnumerable<admConceptos> TraerPorDocumentoModeloId(int documentoModeloId)
     {
-        return _context.admConceptos.Where(concepto => concepto.CIDDOCUMENTODE == documentoModeloId).ToList();
+        return _context.admConceptos.WithSpecification(new ConceptosPorDocumentoModeloIdSpecification(documentoModeloId)).ToList();
     }
 
     /// <inheritdoc />
---
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Specifications/ConceptoSpecifications.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.Specification;
+using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+
+namespace ARSoftware.Contpaqi.Comercial.Sql.Specifications;
+
+public sealed class ConceptoPorCodigoSpecification : SingleResultSpecification<admConceptos>
+{
+    public ConceptoPorCodigoSpecification(string codigoConcepto)
+    {
+        Query.Where(concepto => concepto.CCODIGOCONCEPTO == codigoConcepto);
+    }
+}
+
+public sealed class ConceptoPorIdSpecification : SingleResultSpecification<admConceptos>
+{
+    public ConceptoPorIdSpecification(int idConcepto)
+    {
+        Query.Where(concepto => concepto.CIDCONCEPTODOCUMENTO == idConcepto);
+    }
+}
+
+public sealed class ConceptosPorDocumentoModeloIdSpecification : Specification<admConceptos>
+{
+    public ConceptosPorDocumentoModeloIdSpecification(int documentoModeloId)
+    {
+        Query.Where(concepto => concepto.CIDDOCUMENTODE == documentoModeloId);
+    }
+}
