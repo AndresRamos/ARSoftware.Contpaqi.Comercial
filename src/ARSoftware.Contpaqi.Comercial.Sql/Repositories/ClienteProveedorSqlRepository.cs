a0f6d362af2888a754c46c83ef1990d97028833f
Andres Ramos <andres@arsoft.net>
2023-07-20 01:24:26 -0500

    feat: crea especificaciones sql de clientes

--- src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ClienteProveedorSqlRepository.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ClienteProveedorSqlRepository.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Ardalis.Specification.EntityFrameworkCore;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Enums;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
 using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
 using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+using ARSoftware.Contpaqi.Comercial.Sql.Specifications;
 
 namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
 
-public sealed class ClienteProveedorSqlRepository : IClienteProveedorRepository<admClientes>
+public sealed class ClienteProveedorSqlRepository : RepositoryBase<admClientes>, IClienteProveedorRepository<admClientes>
 {
     private readonly ContpaqiComercialEmpresaDbContext _context;
 
-    public ClienteProveedorSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    public ClienteProveedorSqlRepository(ContpaqiComercialEmpresaDbContext context) : base(context)
     {
         _context = context;
     }
@@ -19,35 +21,31 @@ public sealed class ClienteProveedorSqlRepository : IClienteProveedorRepository<
     /// <inheritdoc />
     public admClientes BuscarPorCodigo(string codigoCliente)
     {
-        return _context.admClientes.SingleOrDefault(cliente => cliente.CCODIGOCLIENTE == codigoCliente);
+        return _context.admClientes.WithSpecification(new ClientePorCodigoSpecification(codigoCliente)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public admClientes BuscarPorId(int idCliente)
     {
-        return _context.admClientes.SingleOrDefault(cliente => cliente.CIDCLIENTEPROVEEDOR == idCliente);
+        return _context.admClientes.WithSpecification(new ClientePorIdSpecification(idCliente)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public IEnumerable<admClientes> TraerClientes()
     {
-        return _context.admClientes.Where(cliente =>
-                cliente.CTIPOCLIENTE == (int)TipoCliente.Cliente || cliente.CTIPOCLIENTE == (int)TipoCliente.ClienteProveedor)
-            .ToList();
+        return _context.admClientes.WithSpecification(new ClientesSpecification()).ToList();
     }
 
     /// <inheritdoc />
     public IEnumerable<admClientes> TraerPorTipo(TipoCliente tipoCliente)
     {
-        return _context.admClientes.Where(cliente => cliente.CTIPOCLIENTE == (int)tipoCliente).ToList();
+        return _context.admClientes.WithSpecification(new ClientesPorTipoSpecification(tipoCliente)).ToList();
     }
 
     /// <inheritdoc />
     public IEnumerable<admClientes> TraerProveedores()
     {
-        return _context.admClientes.Where(cliente =>
-                cliente.CTIPOCLIENTE == (int)TipoCliente.Proveedor || cliente.CTIPOCLIENTE == (int)TipoCliente.ClienteProveedor)
-            .ToList();
+        return _context.admClientes.WithSpecification(new ProveedoresSpecification()).ToList();
     }
 
     /// <inheritdoc />
---
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Specifications/ClienteProveedorSpecifications.cs
@@ -0,0 +1,47 @@
+﻿using Ardalis.Specification;
+using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Enums;
+using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+
+namespace ARSoftware.Contpaqi.Comercial.Sql.Specifications;
+
+public sealed class ClientePorCodigoSpecification : SingleResultSpecification<admClientes>
+{
+    public ClientePorCodigoSpecification(string codigoCliente)
+    {
+        Query.Where(cliente => cliente.CCODIGOCLIENTE == codigoCliente);
+    }
+}
+
+public sealed class ClientePorIdSpecification : SingleResultSpecification<admClientes>
+{
+    public ClientePorIdSpecification(int idCliente)
+    {
+        Query.Where(cliente => cliente.CIDCLIENTEPROVEEDOR == idCliente);
+    }
+}
+
+public sealed class ClientesSpecification : Specification<admClientes>
+{
+    public ClientesSpecification()
+    {
+        Query.Where(cliente =>
+            cliente.CTIPOCLIENTE == (int)TipoCliente.Cliente || cliente.CTIPOCLIENTE == (int)TipoCliente.ClienteProveedor);
+    }
+}
+
+public sealed class ClientesPorTipoSpecification : Specification<admClientes>
+{
+    public ClientesPorTipoSpecification(TipoCliente tipoCliente)
+    {
+        Query.Where(cliente => cliente.CTIPOCLIENTE == (int)tipoCliente);
+    }
+}
+
+public sealed class ProveedoresSpecification : Specification<admClientes>
+{
+    public ProveedoresSpecification()
+    {
+        Query.Where(cliente =>
+            cliente.CTIPOCLIENTE == (int)TipoCliente.Proveedor || cliente.CTIPOCLIENTE == (int)TipoCliente.ClienteProveedor);
+    }
+}
