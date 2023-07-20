18ecc94ec78ace57f814d6f03a37fb6beea70ef6
Andres Ramos <andres@arsoft.net>
2023-07-20 01:50:31 -0500

    feat(sql): crea especificaciones sql de productos

--- src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ParametrosSqlRepository.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ParametrosSqlRepository.cs
@@ -1,15 +1,16 @@
 ﻿using System.Linq;
+using Ardalis.Specification.EntityFrameworkCore;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
 using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
 using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
 
 namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
 
-public sealed class ParametrosSqlRepository : IParametrosRepository<admParametros>
+public sealed class ParametrosSqlRepository : RepositoryBase<admParametros>, IParametrosRepository<admParametros>
 {
     private readonly ContpaqiComercialEmpresaDbContext _context;
 
-    public ParametrosSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    public ParametrosSqlRepository(ContpaqiComercialEmpresaDbContext context) : base(context)
     {
         _context = context;
     }
--- src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ProductoSqlRepository.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/ProductoSqlRepository.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Ardalis.Specification.EntityFrameworkCore;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Enums;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
 using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
 using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+using ARSoftware.Contpaqi.Comercial.Sql.Specifications;
 
 namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
 
-public sealed class ProductoSqlRepository : IProductoRepository<admProductos>
+public sealed class ProductoSqlRepository : RepositoryBase<admProductos>, IProductoRepository<admProductos>
 {
     private readonly ContpaqiComercialEmpresaDbContext _context;
 
-    public ProductoSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    public ProductoSqlRepository(ContpaqiComercialEmpresaDbContext context) : base(context)
     {
         _context = context;
     }
@@ -19,19 +21,19 @@ public sealed class ProductoSqlRepository : IProductoRepository<admProductos>
     /// <inheritdoc />
     public admProductos BuscarPorCodigo(string codigoProducto)
     {
-        return _context.admProductos.SingleOrDefault(producto => producto.CCODIGOPRODUCTO == codigoProducto);
+        return _context.admProductos.WithSpecification(new ProductoPorCodigoSpecification(codigoProducto)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public admProductos BuscarPorId(int idProducto)
     {
-        return _context.admProductos.SingleOrDefault(producto => producto.CIDPRODUCTO == idProducto);
+        return _context.admProductos.WithSpecification(new ProductoPorIdSpecification(idProducto)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public IEnumerable<admProductos> TraerPorTipo(TipoProducto tipoProducto)
     {
-        return _context.admProductos.Where(producto => producto.CTIPOPRODUCTO == (int)tipoProducto).ToList();
+        return _context.admProductos.WithSpecification(new ProductosPorTipoSpecification(tipoProducto)).ToList();
     }
 
     /// <inheritdoc />
