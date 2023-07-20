acb9e7d24da658fcda28245f42c3a0a1280e69a7
Andres Ramos <andres@arsoft.net>
2023-07-19 23:26:30 -0500

    feat(sql): crea specificaiones de agentes

---
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Specifications/AgenteSpecifications.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.Specification;
+using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+
+namespace ARSoftware.Contpaqi.Comercial.Sql.Specifications;
+
+/// <summary>
+///     Busca un agente por su código.
+/// </summary>
+public sealed class AgentePorCodigoSpecification : SingleResultSpecification<admAgentes>
+{
+    public AgentePorCodigoSpecification(string codigoAgente)
+    {
+        Query.Where(agente => agente.CCODIGOAGENTE == codigoAgente);
+    }
+}
+
+/// <summary>
+///     Busca un agente por su id.
+/// </summary>
+public sealed class AgentePorIdSpecification : SingleResultSpecification<admAgentes>
+{
+    public AgentePorIdSpecification(int idAgente)
+    {
+        Query.Where(agente => agente.CIDAGENTE == idAgente);
+    }
+}
