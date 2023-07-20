54413b81960bc77bcf7615e44db31afbcdd971c4
Andres Ramos <andres@arsoft.net>
2023-07-20 00:59:10 -0500

    refactor: aplica especificaciones a AgenteSqlRepository

--- src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/AgenteSqlRepository.cs
+++ src/ARSoftware.Contpaqi.Comercial.Sql/Repositories/AgenteSqlRepository.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Ardalis.Specification.EntityFrameworkCore;
 using ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Repositories;
 using ARSoftware.Contpaqi.Comercial.Sql.Contexts;
 using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
+using ARSoftware.Contpaqi.Comercial.Sql.Specifications;
 
 namespace ARSoftware.Contpaqi.Comercial.Sql.Repositories;
 
-public sealed class AgenteSqlRepository : IAgenteRepository<admAgentes>
+public sealed class AgenteSqlRepository : RepositoryBase<admAgentes>, IAgenteRepository<admAgentes>
 {
     private readonly ContpaqiComercialEmpresaDbContext _context;
 
-    public AgenteSqlRepository(ContpaqiComercialEmpresaDbContext context)
+    public AgenteSqlRepository(ContpaqiComercialEmpresaDbContext context) : base(context)
     {
         _context = context;
     }
@@ -18,13 +20,13 @@ public sealed class AgenteSqlRepository : IAgenteRepository<admAgentes>
     /// <inheritdoc />
     public admAgentes BuscarPorCodigo(string codigoAgente)
     {
-        return _context.admAgentes.SingleOrDefault(agente => agente.CCODIGOAGENTE == codigoAgente);
+        return _context.admAgentes.WithSpecification(new AgentePorCodigoSpecification(codigoAgente)).SingleOrDefault();
     }
 
     /// <inheritdoc />
     public admAgentes BuscarPorId(int idAgente)
     {
-        return _context.admAgentes.SingleOrDefault(agente => agente.CIDAGENTE == idAgente);
+        return _context.admAgentes.WithSpecification(new AgentePorIdSpecification(idAgente)).SingleOrDefault();
     }
 
     /// <inheritdoc />
