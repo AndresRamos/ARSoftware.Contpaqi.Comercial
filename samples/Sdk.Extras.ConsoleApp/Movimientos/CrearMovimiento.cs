ebc7f88a5a406b92c891134129815c0688c13e0f
Andres Ramos <andres@arsoft.net>
2023-08-01 00:19:21 -0500

    chore: agrega ejemplo de como crear un movimiento con descuento

--- samples/Sdk.Extras.ConsoleApp/Movimientos/CrearMovimiento.cs
+++ samples/Sdk.Extras.ConsoleApp/Movimientos/CrearMovimiento.cs
@@ -1,5 +1,6 @@
 ﻿using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Interfaces;
 using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Models;
+using ARSoftware.Contpaqi.Comercial.Sdk.Extras.Models.ValueObjects;
 using ARSoftware.Contpaqi.Comercial.Sql.Models.Empresa;
 
 namespace Sdk.Extras.ConsoleApp;
@@ -23,6 +24,36 @@ public class CrearMovimiento
             Precio = 100,
             Referencia = "Referencia",
             Observaciones = "Observaciones"
+        };
+
+        movimiento.DatosExtra.Add(nameof(admMovimientos.CTEXTOEXTRA1), "Texto Extra 1");
+
+        return _movimientoService.Crear(documentoId, movimiento);
+    }
+
+    public int CrearMovimientoDescuento(int documentoId)
+    {
+        var movimiento = new Movimiento
+        {
+            Producto = new Producto { Codigo = "PRUEBA" },
+            Almacen = new Almacen { Codigo = "PRUEBA" },
+            Unidades = 1,
+            Precio = 100,
+            Referencia = "Referencia",
+            Observaciones = "Observaciones",
+            Descuentos = new DescuentosMovimiento
+            {
+                Descuento1 =
+                {
+                    Tasa = 10 // El porcentaje de descuento = 10%
+                    // Importe = 10 // Se puede asignar el importe y/o la tasa
+                }
+                // Se pueden asignar hasta 5 descuentos dependiendo de la configuracion del concepto
+                //Descuento2 = { Tasa = 10 },
+                //Descuento3 = { Tasa = 10 },
+                //Descuento4 = { Tasa = 10 },
+                //Descuento5 = { Tasa = 10 },
+            }
         };
 
         movimiento.DatosExtra.Add(nameof(admMovimientos.CTEXTOEXTRA1), "Texto Extra 1");
--- samples/Sdk.Extras.ConsoleApp/Program.cs
+++ samples/Sdk.Extras.ConsoleApp/Program.cs
@@ -36,8 +36,8 @@ try
 
     // 1. Busca la clase con los ejemplos que quieras probar utilizando el proveedor de servicios.
     // 2. Ejecuta el metodo que quieras probar.
-    var ejemplo = host.Services.GetRequiredService<BuscarClientesConRepositorio>();
-    ejemplo.TraerTodo();
+    var ejemplo = host.Services.GetRequiredService<CrearFactura>();
+    ejemplo.Crear();
 }
 catch (Exception e)
 {
