1c62b925d595d959b27e14647b0f2435077f06fc
Andres Ramos <andres@arsoft.net>
2023-11-05 18:41:29 -0600

    Crea ControlExistencias flags enum

---
+++ src/ARSoftware.Contpaqi.Comercial.Sdk.Abstractions/Enums/ControlExistencias.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ARSoftware.Contpaqi.Comercial.Sdk.Abstractions.Enums;
+
+[Flags]
+public enum ControlExistencias
+{
+    Ninguno = 0,
+    Unidades = 1,
+    Caracteristicas = 2,
+    Series = 4,
+    Pedimentos = 8,
+    Lotes = 16
+}
